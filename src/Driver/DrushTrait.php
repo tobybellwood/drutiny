67f7cafa7f5dc34e24cb18e1e5d23fca6bd625be
Josh Waihi <josh.waihi@acquia.com>
2017-11-02 09:52:43 +1300

    Bit of logging to note if a drush command failed

--- src/Driver/DrushTrait.php
+++ src/Driver/DrushTrait.php
@@ -28,6 +28,7 @@ trait DrushTrait {
       $output = $this->runCommand($method, $args);
     }
     catch (ProcessFailedException $e) {
+      $this->sandbox()->logger()->warning($e->getProcess()->getOutput());
       throw new DrushFormatException("Drush command failed.", $e->getProcess()->getOutput());
     }
 
